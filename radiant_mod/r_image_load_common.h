ba8c4cf908533537ab9708ea9d5ada6bd3d0b045
SE2Dev <brokensilencedev@gmail.com>
2015-02-21 19:27:15 -0500

    Add Image_Reload Function
    Deleted xmodelsurfs_info.txt

--- radiant_mod/r_image_load_common.h
+++ radiant_mod/r_image_load_common.h
@@ -105,3 +105,5 @@ D3DCUBEMAP_FACES Image_CubemapFace(unsigned int faceIndex);
 void Image_Setup(GfxImage *image, int width, int height, int depth, unsigned int imageFlags, D3DFORMAT imageFormat);
 void Image_Upload2D_CopyData_PC(GfxImage *image, D3DFORMAT format, D3DCUBEMAP_FACES face, unsigned int mipLevel, const char *src);
 void Image_Upload3D_CopyData_PC(GfxImage *image, D3DFORMAT format, unsigned int mipLevel, const char *src);
+
+void Image_Reload();
