79eec97ffae48e47c9840ee46f3f8615c46351a5
SE2Dev <brokensilencedev@gmail.com>
2015-03-30 18:51:24 -0400

    Add SP_Tool Addresses for Com_Memory

---
+++ paths_mod/com_memory.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+
+SRCLINE(1220)
+void *Hunk_Alloc(int size, const char *name, int type)
+{
+	return Z_Malloc(size);
+}
+
+SRCLINE(1894)
+void Z_Free(void *ptr)
+{
+	((void(__cdecl *)(void *))0x006C48AF)(ptr);
+}
+
+SRCLINE(1913)
+void *Z_Malloc(int size)
+{
+	return ((void *(__cdecl *)(int))0x00554A6D)(size);
+}
