80120922fd02129df1d8d6b162bed294cba5ac7d
SE2Dev <brokensilencedev@gmail.com>
2015-09-02 16:39:00 -0400

    Add Progress Counters for Compiling Reflections

--- components/game_mod/reflection_fix.h
+++ components/game_mod/reflection_fix.h
@@ -2,6 +2,12 @@
 #include <Windows.h>
 
 BOOL IsReflectionMode();
+BOOL IsInjectionMode();
 BOOL ReflectionMod_Init();
 
+void hk_R_GenerateReflectionRawDataAll();
+void __cdecl R_GenerateReflectionRawDataAll(DiskGfxReflectionProbe *probeRawData, int probeCount, bool *generateProbe);
 void __cdecl R_GenerateReflectionRawData(DiskGfxReflectionProbe* probeRawData);
+
+BOOL ReflectionsWereUpdated();
+BOOL InjectReflections();
