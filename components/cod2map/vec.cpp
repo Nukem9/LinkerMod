c6fe7235c751e32d460d61d8e425dfb6e0eb9ddc
SE2Dev <brokensilencedev@gmail.com>
2016-01-21 15:32:54 -0500

    Add Support for BO1 Specific Primary Light KVs

---
+++ components/cod2map/vec.cpp
@@ -0,0 +1,17 @@
+#include "vec.h"
+
+vec3::vec3(void)
+{
+}
+
+vec3::vec3(float a, float b, float c) : x(a), y(b), z(c)
+{
+}
+
+vec4::vec4(void)
+{
+}
+
+vec4::vec4(float a, float b, float c, float d) : x(a), y(b), z(c), w(d)
+{
+}
