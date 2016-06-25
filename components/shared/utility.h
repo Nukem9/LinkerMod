4a16f397281be1a7705a8cb50ba262b3e1feb4c1
SE2Dev <brokensilencedev@gmail.com>
2016-06-24 21:01:10 -0400

    Add Logging Detour Function

--- components/shared/utility.h
+++ components/shared/utility.h
@@ -50,3 +50,80 @@ static bool StrEndsWith (std::string str, std::string substr)
 	else
         return false;
 }
+
+namespace Detours
+{
+	namespace X86
+	{
+		class LogHook
+		{
+		private:
+			void* rtn;
+			BYTE* op;
+			void* fnc;
+			char* msg;
+
+		public:
+			LogHook(const char* message)
+			{
+				op = new BYTE[23];
+				PatchMemory((ULONG_PTR)op + 0,	(PBYTE)"\x60", 1);						//pusha
+				PatchMemory((ULONG_PTR)op + 1,	(PBYTE)"\xFF\x35\x00\x00\x00\x00", 6);	//push str
+				PatchMemory((ULONG_PTR)op + 7,	(PBYTE)"\xFF\x15\x00\x00\x00\x00", 6);	//call printf
+				PatchMemory((ULONG_PTR)op + 13,	(PBYTE)"\x83\xC4\x04", 3);				//add esp, 4
+				PatchMemory((ULONG_PTR)op + 16,	(PBYTE)"\x61", 1);						//popa
+				PatchMemory((ULONG_PTR)op + 17,	(PBYTE)"\xFF\x25\x00\x00\x00\x00", 6);	//jmp rtn
+
+				this->msg = (char*)message;
+				this->fnc = (void*)&PrintMessage;
+				void** str = (void**)&msg;
+				
+				void** prnt = &fnc;
+				void** retn = &rtn;
+
+				PatchMemory((ULONG_PTR)op + 3,	(PBYTE)&str, 4);	//fix str
+				PatchMemory((ULONG_PTR)op + 9,	(PBYTE)&prnt, 4);	//fix printf
+				PatchMemory((ULONG_PTR)op + 19,	(PBYTE)&retn, 4);	//fix rtn
+			}
+
+			~LogHook()
+			{
+				delete[] op;
+			}
+
+			static void __cdecl PrintMessage(const char* str)
+			{
+				printf("%s\n", str);
+			}
+
+			friend void AddLogHook(uint8_t * target, const char* msg);
+			friend static void RemoveLogHook(uint8_t * target);
+		};
+
+		// Print a message when the code at the target address is executed
+		static void AddLogHook(uint8_t * target, const char* msg)
+		{
+			LogHook* hk = new LogHook(msg);
+			hk->rtn = DetourFunction(target, hk->op, Detours::X86Option::USE_JUMP);
+		}
+
+		static void* GetDest_JmpRel32(BYTE* instr)
+		{
+			ASSERT(*instr == 0xE9);
+			int* target_rel32 = (int*)(instr + 1); 
+			return instr + 5 + *target_rel32; // jmp instruction offset + the relative offset that it would jump to + size of jmp instruction
+		}
+
+		// Remove a log hook and restore the original code, where target matches the target argument used in AddLogHook
+		static void RemoveLogHook(uint8_t * target)
+		{
+			void* trampoline = GetDest_JmpRel32(target);
+			BYTE* op = (BYTE*)GetDest_JmpRel32((BYTE*)trampoline);
+
+			LogHook* hk = *(LogHook**)(op + 19);
+
+			DetourRemove((PBYTE)hk->rtn);
+			delete[] hk;
+		}
+	}
+}
