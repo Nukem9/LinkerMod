864ce31f616452ea4373595af99aabb9a4b5ebb1
Nukem <Nukem@outlook.com>
2016-10-05 23:45:24 -0400

    Fix if() logic and possible buffer overflow in Cinematic_BinkOpen

--- components/game_mod/r_cinematic.cpp
+++ components/game_mod/r_cinematic.cpp
@@ -15,7 +15,8 @@ void __declspec(naked) hk_R_Cinematic_BinkOpen(void)
 
 bool __cdecl R_Cinematic_BinkOpen(const char *filename, unsigned int playbackFlags, char *errText)
 {
-	char filepath[256] = "\0";
+	char filepath[256];
+	memset(filepath, 0, sizeof(filepath));
 
 	const char* cwd = Sys_Cwd();
 	
@@ -23,37 +24,32 @@ bool __cdecl R_Cinematic_BinkOpen(const char *filename, unsigned int playbackFla
 	// Attempt to load any custom loadscreens before the stock ones (Allows for mod-specific loadscreens for stock maps)
 	//
 	const char* fs_game = Dvar_GetString("fs_game");
-	if (fs_game || fs_game[0] != '\0')
+	if (fs_game && fs_game[0] != '\0')
 	{
-		_snprintf_s(filepath, 256, "%s\\%s\\video\\%s.%s", cwd, fs_game, filename, "bik");
-		if (filepath[0] && R_Cinematic_BinkOpenPath(filepath, playbackFlags, errText))
-			return 1;
+		_snprintf_s(filepath, _TRUNCATE, "%s\\%s\\video\\%s.bik", cwd, fs_game, filename);
+		if (R_Cinematic_BinkOpenPath(filepath, playbackFlags, errText))
+			return true;
 	}
 
 	if (playbackFlags & 8)
 	{
-		_snprintf_s(filepath, 256, "bik/%s.%s", filename, "bik");
+		_snprintf_s(filepath, _TRUNCATE, "bik/%s.bik", filename);
 	}
 	else
 	{
-		_snprintf_s(filepath, 256, "%s\\main\\video\\%s.%s", cwd, filename, "bik");
+		_snprintf_s(filepath, _TRUNCATE, "%s\\main\\video\\%s.bik", cwd, filename);
 	}
 
 	if (R_Cinematic_BinkOpenPath(filepath, playbackFlags, errText))
-		return 1;
+		return true;
 
-	_snprintf_s(filepath, 256, "%s\\raw\\video\\%s.%s", cwd, filename, "bik");
-	if (filepath[0] && R_Cinematic_BinkOpenPath(filepath, playbackFlags, errText))
-		return 1;
-
-	return 0;
+	_snprintf_s(filepath, _TRUNCATE, "%s\\raw\\video\\%s.bik", cwd, filename);
+	return R_Cinematic_BinkOpenPath(filepath, playbackFlags, errText);
 }
 
-bool __cdecl R_Cinematic_BinkOpenPath(const char *filepath, unsigned int playbackFlags, char *errText)
+bool R_Cinematic_BinkOpenPath(const char *filepath, unsigned int playbackFlags, char *errText)
 {
-	bool result = 0;
-
-	_asm
+	__asm
 	{
 		push errText
 		push playbackFlags
@@ -62,9 +58,6 @@ bool __cdecl R_Cinematic_BinkOpenPath(const char *filepath, unsigned int playbac
 		mov ebx, 0x006D96E0
 		call ebx
 		add esp, 8
-		mov result, al
 	}
-
-	return result;
 }
 
--- components/game_mod/r_cinematic.h
+++ components/game_mod/r_cinematic.h
@@ -3,4 +3,4 @@
 void hk_R_Cinematic_BinkOpen(void);
 bool __cdecl R_Cinematic_BinkOpen(const char *filename, unsigned int playbackFlags, char *errText);
 
-bool __cdecl R_Cinematic_BinkOpenPath(const char *filepath, unsigned int playbackFlags, char *errText);
+bool R_Cinematic_BinkOpenPath(const char *filepath, unsigned int playbackFlags, char *errText);
