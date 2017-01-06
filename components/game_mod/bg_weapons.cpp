9c9968c6a1309744dc68367dc6b606ec18aeb361
Nukem <Nukem@outlook.com>
2017-01-06 17:17:05 -0500

    game_mod: Disable weapon jamming

--- components/game_mod/bg_weapons.cpp
+++ components/game_mod/bg_weapons.cpp
@@ -7,3 +7,8 @@ void hk_PM_WeaponUseAmmo(int ps, int wp, int amount)
 	if (!player_sustainAmmo->current.enabled)
 		PM_WeaponUseAmmo(ps, wp, amount);
 }
+
+void PM_Weapon_Jam(int ps)
+{
+	// Do nothing. ESI is argument.
+}
--- components/game_mod/bg_weapons.h
+++ components/game_mod/bg_weapons.h
@@ -3,3 +3,4 @@
 extern void(*PM_WeaponUseAmmo)(int ps, int wp, int amount);
 
 void hk_PM_WeaponUseAmmo(int ps, int wp, int amount);
+void PM_Weapon_Jam(int ps);
