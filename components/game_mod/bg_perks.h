e355010e89a69e353fab59c5193d59737ba4d7e3
Nukem <Nukem@outlook.com>
2017-01-14 06:05:21 -0500

    game_mod: Add perk functions for future use

--- components/game_mod/bg_perks.h
+++ components/game_mod/bg_perks.h
@@ -1,12 +1,39 @@
 #pragma once
 
 #define PERK_DOUBLETAP 10
+#define PERK_COUNT 15
 
 // /game/bg_perks.h:135
 static bool BG_HasPerk(const unsigned int *perks, unsigned int perkIndex)
 {
 	ASSERT(perks);
-	ASSERT(perkIndex >= 0 && perkIndex < 15);
+	ASSERT(perkIndex >= 0 && perkIndex < PERK_COUNT);
 
-	return (*perks & (3 << 2 * perkIndex)) != 0;
+	return (perks[0] & (3 << 2 * perkIndex)) != 0;
+}
+
+// /game/bg_perks.h:144
+static void BG_SetPerk(unsigned int *perks, unsigned int perkIndex)
+{
+	ASSERT(perks);
+	ASSERT(perkIndex >= 0 && perkIndex < PERK_COUNT);
+
+	perks[0] |= (3 << 2 * perkIndex);
+}
+
+// /game/bg_perks.h:159
+static void BG_UnsetPerk(unsigned int *perks, unsigned int perkIndex)
+{
+	ASSERT(perks);
+	ASSERT(perkIndex >= 0 && perkIndex < PERK_COUNT);
+
+	perks[0] &= ~(3 << 2 * perkIndex);
+}
+
+// /game/bg_perks.h:174
+static void BG_ClearPerks(unsigned int *perks)
+{
+	ASSERT(perks);
+
+	perks[0] = 0;
 }
