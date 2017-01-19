790c39297f0f2122dc6d93f296767f35efc13878
SE2Dev <brokensilencedev@gmail.com>
2017-01-19 11:59:17 -0500

    Initial Rewrite of Various Lightgrid Functions

--- components/cod2rad/r_lightgrid.cpp
+++ components/cod2rad/r_lightgrid.cpp
@@ -152,3 +152,212 @@ void __declspec(naked) hk_R_Store_QuantizedLightGridSample()
 }
 
 #endif
+
+void __cdecl AdjustLightingContrast(int sampleCount, int baseIndex, vec3* colors)
+{
+	_asm
+	{
+		push colors
+		push baseIndex
+		mov edi, sampleCount
+		mov ebx, 0x00430410
+		call ebx
+		add esp, 8
+	}
+}
+
+// 0x00434E20
+void __cdecl StoreLightingForDir(vec3* lighting, BYTE* dst)
+{
+	for (int i = 0; i < 56; i++)
+	{
+		GammaCorrectColor((float*)&lighting[i]);
+	}
+
+	AdjustLightingContrast(56, -1, lighting);
+
+	for (int sampleIndex = 0; sampleIndex < 56; sampleIndex++)
+	{
+		vec3 v = lighting[sampleIndex] * 0.5;
+		dst[sampleIndex * 3 + 0] = EncodeFloatInByte(v.r);
+		dst[sampleIndex * 3 + 1] = EncodeFloatInByte(v.g);
+		dst[sampleIndex * 3 + 2] = EncodeFloatInByte(v.b);
+	}
+}
+
+void CalculateClusterMeanAndVariance(GridColorsCluster *Cluster)
+{
+	ASSERT(Cluster);
+	ASSERT(Cluster->count);
+
+	static DWORD dwCall = 0x00434320;
+
+	__asm
+	{
+		mov edi, Cluster
+			call[dwCall]
+	}
+}
+
+GridColorsCluster *ChooseClusterToSplit()
+{
+	static DWORD dwCall = 0x00433090;
+
+	__asm
+	{
+		call[dwCall]
+	}
+}
+
+void SplitCluster(GridColorsCluster *Cluster)
+{
+	ASSERT(Cluster);
+	ASSERT(Cluster->count >= 2);
+
+	static DWORD dwCall = 0x00434B50;
+
+	__asm
+	{
+		mov esi, Cluster
+			call[dwCall]
+	}
+}
+
+void SwapClusters(int FromIndex, int ToIndex)
+{
+	static DWORD dwCall = 0x00432EF0;
+
+	__asm
+	{
+		push FromIndex
+			push ToIndex
+			call[dwCall]
+			add esp, 0x8
+	}
+}
+
+int GetClusterDefaultScore(GridColorsCluster *Cluster)
+{
+	static DWORD dwCall = 0x00432E90;
+
+	__asm
+	{
+		mov ecx, Cluster
+			call[dwCall]
+	}
+}
+
+void CalculateClusterMean(GridColorsCluster *Cluster, float *Means)
+{
+	static DWORD dwCall = 0x004331A0;
+
+	__asm
+	{
+		mov ecx, Cluster
+		mov eax, Means
+		call[dwCall]
+	}
+}
+
+void SetLightGridColorsForCluster(GridColorsCluster *Cluster, GfxLightGridColors *Colors)
+{
+	float means[168];
+	CalculateClusterMean(Cluster, means);
+
+	int v2 = 0;
+	do
+	{
+		float v3 = means[v2++];
+		int v6 = (int)(v3 + 0.5);
+
+		Colors->all[v2 - 1] = v6;
+	} while (v2 < 168);
+}
+
+void __cdecl ImproveLightGridValues(int threadCount)
+{
+	_asm
+	{
+		mov eax, threadCount
+		mov ebx, 0x00435830
+		call ebx
+	}
+}
+
+void ClusterLightGridValues(int ThreadCount)
+{
+	lightGridGlob->mapping = new unsigned int[lightGridGlob->pointCount];
+
+	if (!lightGridGlob->mapping)
+		Com_FatalError("Couldn't allocate %i bytes for light grid color mapping", sizeof(int) * lightGridGlob->pointCount);
+
+	lightGridGlob->clusters = new GridColorsCluster[lightGridColorCount];
+
+	if (!lightGridGlob->clusters)
+		Com_FatalError("Couldn't allocate %i bytes for light grid colors", sizeof(GridColorsCluster) * lightGridColorCount);
+
+	for (unsigned int i = 0; i < lightGridGlob->pointCount; ++i)
+		lightGridGlob->mapping[i] = i;
+
+	lightGridGlob->clusterCount = 1;
+	lightGridGlob->clusters[0].first = 0;
+	lightGridGlob->clusters[0].count = lightGridGlob->pointCount;
+	CalculateClusterMeanAndVariance(&lightGridGlob->clusters[0]);
+
+	unsigned int j = lightGridGlob->clusterCount;
+	for (; lightGridGlob->clusterCount < lightGridColorCount; j = lightGridGlob->clusterCount)
+	{
+		GridColorsCluster *cluster = ChooseClusterToSplit();
+
+		if (*(float *)0x153C902C >= cluster->unknown3 && j >= 2)
+			break;
+
+		SplitCluster(cluster);
+	}
+
+	unsigned int maxScore = 0;
+	unsigned int maxScoreIndex = 0;
+
+	if (j > 0)
+	{
+		GfxLightGridColors *lightGridColors = (GfxLightGridColors *)0x96CAE08;
+
+		for (unsigned int i = 0; i < lightGridGlob->clusterCount; i++)
+		{
+			GridColorsCluster *cluster = &lightGridGlob->clusters[i];
+			unsigned int firstIndex = cluster->first;
+
+			if (firstIndex < (firstIndex + cluster->count))
+			{
+				do
+					*(WORD *)(lightGridGlob->points + 12 * lightGridGlob->mapping[firstIndex++] + 8) = i;
+				while (firstIndex < (firstIndex + cluster->count));
+			}
+
+			SetLightGridColorsForCluster(cluster, lightGridColors);
+
+			unsigned int score = GetClusterDefaultScore(cluster);
+			if (score > maxScore)
+			{
+				maxScore = score;
+				maxScoreIndex = i;
+			}
+
+			lightGridColors++;
+		}
+	}
+
+	SwapClusters(0, maxScoreIndex);
+	SwapClusters(1, *(WORD *)(lightGridGlob->points + 12 * lightGridGlob->pointCount - 4));
+	lightGridColorCount = lightGridGlob->clusterCount;
+
+	if (options_ImproveLights)
+		ImproveLightGridValues(ThreadCount);
+
+	delete[] lightGridGlob->clusters;
+	delete[] lightGridGlob->mapping;
+	lightGridGlob->clusters = nullptr;
+	lightGridGlob->mapping = nullptr;
+
+	--lightGridGlob->pointCount;
+}
--- components/cod2rad/r_lightgrid.h
+++ components/cod2rad/r_lightgrid.h
@@ -3,6 +3,57 @@
 const static DWORD* g_lightgridSampleCount = (DWORD*)0x153C91D0;
 const static DWORD* g_diskLightgridSampleCount = (DWORD*)0x153C91E0;
 
+struct GfxLightGridColors
+{
+	union
+	{
+		BYTE rgb[56][3];
+		BYTE all[56 * 3];
+	};
+};
+
+struct GridColorsCluster
+{
+	unsigned int first;		// 0x00
+	unsigned int count;		// 0x04
+	unsigned int unknown1;	// 0x08
+	float unknown2;			// 0x0C
+	float unknown3;			// 0x10
+};
+
+struct GridSamplePoint
+{
+	WORD pos[3];	// 0x00
+	BYTE unknown4;	// 0x06 ???
+	WORD unknown5;	// 0x08 ColorsIndex?
+	BYTE unknown6;	// 0x0A	PrimaryLightIndex?
+	BYTE unknown7;	// 0x0C NeedsTrace?
+};
+
+struct LightGridGlob
+{
+	unsigned int pointCount;		// 0x00 0x153C91D0
+	unsigned int maxPoints;			// 0x04 0x153C91D4
+	unsigned int points;			// 0x08 0x153C91D8
+	unsigned int dword_153C91DC;	// 0x0C 0x153C91DC
+	unsigned int clusterCount;		// 0x10 0x153C91E0
+	GridColorsCluster *clusters;	// 0x14 0x153C91E4
+	unsigned int *mapping;			// 0x18 0x153C91E8
+	unsigned int dword_153C91EC;	// 0x1C 0x153C91EC
+	unsigned int dword_153C91F0;	// 0x20 0x153C91F0
+	float flt_153C91F4;				// 0x24 0x153C91F4
+	float flt_153C91F8;				// 0x28 0x153C91F8
+	float flt_153C91FC;				// 0x2C 0x153C91FC
+	float flt_153C9200;				// 0x30 0x153C9200
+	unsigned int dword_153C9204;	// 0x34 0x153C9204
+	// ...
+};
+
+static LightGridGlob *lightGridGlob = (LightGridGlob *)0x153C91D0;
+
+static unsigned int& lightGridColorCount = *(unsigned int *)0x153C9028;
+static bool& options_ImproveLights = *(bool *)0x153C9005;
+
 #if USE_LEGACY_HDR
 typedef float SampleColorHDR[56][3];
 typedef WORD DiskSampleColorHDR[56][3];
