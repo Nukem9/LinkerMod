5a092b4ce8c95e9e9c074fd051dab1c689dfebbb
Nukem <Nukem@outlook.com>
2017-01-22 04:11:32 -0500

    game_mod: bitarray verified

--- components/game_mod/bitarray.h
+++ components/game_mod/bitarray.h
@@ -3,23 +3,21 @@
 template<size_t BIT_COUNT>
 class bitarray
 {
-	static_assert(BIT_COUNT == 51, "Class doesn't support other bit sizes with current code");
-
 public:
 	enum
 	{
-		BITS_PER_WORD	= 32,
+		BITS_PER_WORD	= sizeof(unsigned int) * 8,
 		WORD_COUNT		= (BIT_COUNT + BITS_PER_WORD - 1) / BITS_PER_WORD,
 	};
 
 	// N bits -> array of size int(N/32) rounded up
-	int array[WORD_COUNT];
+	unsigned int array[WORD_COUNT];
 
 	// /qcommon/bitarray.h:108
 	bool testBit(unsigned int pos)
 	{
 		ASSERT(pos < BIT_COUNT);
 
-		return (this->array[pos >> 5] & (0x80000000 >> (pos & 0x1F))) != 0;
+		return (array[pos / BITS_PER_WORD] & (0x80000000 >> (pos & 0x1F))) != 0;
 	}
 };
