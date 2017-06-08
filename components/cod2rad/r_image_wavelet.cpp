9e2c88882c4de9305c612ea629f06c7ef54042ee
SE2Dev <brokensilencedev@gmail.com>
2017-06-08 12:59:19 -0400

    CoD2Rad: Fix Wavelet Decompression Issues

--- components/cod2rad/r_image_wavelet.cpp
+++ components/cod2rad/r_image_wavelet.cpp
@@ -20,9 +20,9 @@ struct WaveletHuffmanDecode
 	__int16 bits;
 };
 
-static WaveletHuffmanDecode* waveletDecodeBlue = (WaveletHuffmanDecode*)0x0045AD30;
-static WaveletHuffmanDecode* waveletDecodeRedGreen = (WaveletHuffmanDecode*)0x0045ED30;
-static WaveletHuffmanDecode* waveletDecodeAlpha = (WaveletHuffmanDecode*)0x00462D30;
+static WaveletHuffmanDecode* waveletDecodeBlue = (WaveletHuffmanDecode*)0x0044E370;
+static WaveletHuffmanDecode* waveletDecodeRedGreen = (WaveletHuffmanDecode*)0x00452370;
+static WaveletHuffmanDecode* waveletDecodeAlpha = (WaveletHuffmanDecode*)0x00456370;
 
 static void Wavelet_ConsumeBits(unsigned __int16 bitCount, WaveletDecode *decode)
 {
@@ -30,7 +30,12 @@ static void Wavelet_ConsumeBits(unsigned __int16 bitCount, WaveletDecode *decode
 	ASSERT(decode->bit < 8);
 
 	decode->value >>= bitCount;
-	decode->value |= ((*(decode->data + 3) << 24) | (*(decode->data + 2) << 16) | (*(decode->data + 1) << 8) | *decode->data) >> decode->bit << (16 - bitCount);
+
+	decode->value |= (((BYTE)decode->data[0] << 0)	|
+					  ((BYTE)decode->data[1] << 8)	|
+					  ((BYTE)decode->data[2] << 16)	|
+					  ((BYTE)decode->data[3] << 24)) >> decode->bit << (16 - bitCount);
+
 	decode->bit += bitCount;
 	decode->data += decode->bit >> 3;
 	decode->bit &= 7u;
@@ -40,17 +45,17 @@ static int Wavelet_DecodeValue(WaveletHuffmanDecode *decodeTable, unsigned __int
 {
 	ASSERT((1 << bitCount) - 1 >= bias * 2 - 1);
 	ASSERT((1 << (bitCount - 1)) - 1 < bias * 2 - 1);
-
+	
 	int index = decode->value & 0xFFF;
 	Wavelet_ConsumeBits(decodeTable[index].bits, decode);
 	int value = decodeTable[index].value;
-
+	
 	if (value == 0xFFFF8000)
 	{
 		value = (((1 << bitCount) - 1) & decode->value) - bias;
 		Wavelet_ConsumeBits(bitCount, decode);
 	}
-
+	
 	return value;
 }
 
@@ -71,13 +76,7 @@ static void Wavelet_AddDeltaToMipmap(char *inout, int size, WaveletDecode *decod
 
 static void Wavelet_DecompressLevel(char *src, char *dst, WaveletDecode *decode)
 {
-	/*
-	"decode->bpp not in [1, 4]\n\t%i not in [%i, %i]",
-		decode->bpp,
-		1,
-		4
-	*/
-	ASSERT(decode->bpp >= 1 && decode->bpp <= 4);
+	ASSERT_MSG_VA(decode->bpp >= 1 && decode->bpp <= 4, "decode->bpp not in [1, 4]\n\t%i not in [%i, %i]", decode->bpp, 1, 4);
 
 	ASSERT(decode->bpp == decode->channels || (decode->bpp == 4 && decode->channels == 3));
 	ASSERT(decode->mipLevel >= 0);
@@ -112,7 +111,7 @@ static void Wavelet_DecompressLevel(char *src, char *dst, WaveletDecode *decode)
 	{
 		if (!decode->dataInitialized)
 		{
-			decode->value = (*(decode->data + 1) << 8) | *decode->data;
+			decode->value = (((BYTE)decode->data[1]) << 8) | (BYTE)decode->data[0];
 			decode->bit = 0;
 			decode->data += 2;
 			decode->dataInitialized = 1;
@@ -136,13 +135,13 @@ static void Wavelet_DecompressLevel(char *src, char *dst, WaveletDecode *decode)
 				if (decode->channels != 1)
 				{
 					int evenOddParity = decode->value & 1;
-					Wavelet_ConsumeBits(1u, decode);
+					Wavelet_ConsumeBits(1, decode);
 
-					coeff[0][0] = Wavelet_DecodeValue(waveletDecodeBlue, 9u, 255, decode);
-					coeff[0][1] = Wavelet_DecodeValue(waveletDecodeBlue, 9u, 255, decode);
-					coeff[0][2] = Wavelet_DecodeValue(waveletDecodeBlue, 9u, 255, decode);
+					coeff[0][0] = Wavelet_DecodeValue(waveletDecodeBlue, 9, 255, decode);
+					coeff[0][1] = Wavelet_DecodeValue(waveletDecodeBlue, 9, 255, decode);
+					coeff[0][2] = Wavelet_DecodeValue(waveletDecodeBlue, 9, 255, decode);
 
-					int base = 2 * src[dstChanOffset[0]];
+					int base = 2 * (BYTE)src[dstChanOffset[0]];
 					char* dstChan = &dst[dstChanOffset[0]];
 
 					dst[dstChanOffset[0]] = evenOddParity + ((coeff[0][2] + coeff[0][1] + coeff[0][0] + base) >> 1);
@@ -154,13 +153,13 @@ static void Wavelet_DecompressLevel(char *src, char *dst, WaveletDecode *decode)
 					if (decode->channels >= 3)
 					{
 						evenOddParity = decode->value & 1;
-						Wavelet_ConsumeBits(1u, decode);
+						Wavelet_ConsumeBits(1, decode);
 
-						coeff[1][0] = coeff[0][0] + Wavelet_DecodeValue(waveletDecodeRedGreen, 0xAu, 510, decode);
-						coeff[1][1] = coeff[0][1] + Wavelet_DecodeValue(waveletDecodeRedGreen, 0xAu, 510, decode);
-						coeff[1][2] = coeff[0][2] + Wavelet_DecodeValue(waveletDecodeRedGreen, 0xAu, 510, decode);
+						coeff[1][0] = coeff[0][0] + Wavelet_DecodeValue(waveletDecodeRedGreen, 10, 510, decode);
+						coeff[1][1] = coeff[0][1] + Wavelet_DecodeValue(waveletDecodeRedGreen, 10, 510, decode);
+						coeff[1][2] = coeff[0][2] + Wavelet_DecodeValue(waveletDecodeRedGreen, 10, 510, decode);
 
-						base = 2 * src[dstChanOffset[1]];
+						base = 2 * (BYTE)src[dstChanOffset[1]];
 						dstChan = &dst[dstChanOffset[1]];
 
 						dst[dstChanOffset[1]] = evenOddParity + ((coeff[1][2] + coeff[1][1] + coeff[1][0] + base) >> 1);
@@ -170,13 +169,13 @@ static void Wavelet_DecompressLevel(char *src, char *dst, WaveletDecode *decode)
 						dstChan[dstBpp + stride] = (base - coeff[1][0] - (coeff[1][1] - coeff[1][2])) >> 1;
 
 						evenOddParity = decode->value & 1;
-						Wavelet_ConsumeBits(1u, decode);
+						Wavelet_ConsumeBits(1, decode);
 						
-						coeff[2][0] = coeff[0][0] + Wavelet_DecodeValue(waveletDecodeRedGreen, 0xAu, 510, decode);
-						coeff[2][1] = coeff[0][1] + Wavelet_DecodeValue(waveletDecodeRedGreen, 0xAu, 510, decode);
-						coeff[2][2] = coeff[0][2] + Wavelet_DecodeValue(waveletDecodeRedGreen, 0xAu, 510, decode);
+						coeff[2][0] = coeff[0][0] + Wavelet_DecodeValue(waveletDecodeRedGreen, 10, 510, decode);
+						coeff[2][1] = coeff[0][1] + Wavelet_DecodeValue(waveletDecodeRedGreen, 10, 510, decode);
+						coeff[2][2] = coeff[0][2] + Wavelet_DecodeValue(waveletDecodeRedGreen, 10, 510, decode);
 						
-						base = 2 * src[dstChanOffset[2]];
+						base = 2 * (BYTE)src[dstChanOffset[2]];
 						dstChan = &dst[dstChanOffset[2]];
 
 						dst[dstChanOffset[2]] = evenOddParity + ((coeff[2][2] + coeff[2][1] + coeff[2][0] + base) >> 1);
@@ -197,25 +196,22 @@ static void Wavelet_DecompressLevel(char *src, char *dst, WaveletDecode *decode)
 
 						dstChan[dstBpp] = -1;
 						dstChan[stride] = -1;
-						*(&dstChan[dstBpp] + stride) = -1;
+						dstChan[dstBpp + stride] = -1;
 					}
 				}
 				else
 				{
 					int evenOddParity = decode->value & 1;
-					Wavelet_ConsumeBits(1u, decode);
+					Wavelet_ConsumeBits(1, decode);
 
-					coeff[3][0] = Wavelet_DecodeValue(waveletDecodeAlpha, 9u, 255, decode);
-					coeff[3][1] = Wavelet_DecodeValue(waveletDecodeAlpha, 9u, 255, decode);
-					coeff[3][2] = Wavelet_DecodeValue(waveletDecodeAlpha, 9u, 255, decode);
+					coeff[3][0] = Wavelet_DecodeValue(waveletDecodeAlpha, 9, 255, decode);
+					coeff[3][1] = Wavelet_DecodeValue(waveletDecodeAlpha, 9, 255, decode);
+					coeff[3][2] = Wavelet_DecodeValue(waveletDecodeAlpha, 9, 255, decode);
 
-					// TODO - Validate this
-					//int base = 2 * src[*(&v9 + decode->channels)];
-					//char* dstChan = &dst[*(&v9 + decode->channels)];
-					int base = 2 * src[dstChanOffset[decode->channels]];
-					char* dstChan = &dst[dstChanOffset[decode->channels]];
+					int base = 2 * src[dstChanOffset[decode->channels - 1]];
+					char* dstChan = &dst[dstChanOffset[decode->channels - 1]];
 
-					*dstChan = evenOddParity + ((coeff[3][2] + coeff[3][1] + coeff[3][0] + base) >> 1);
+					dstChan[0] = evenOddParity + ((coeff[3][2] + coeff[3][1] + coeff[3][0] + base) >> 1);
 
 					dstChan[dstBpp] = (coeff[3][0] + base - (coeff[3][2] + coeff[3][1])) >> 1;
 					dstChan[stride] = (coeff[3][1] - coeff[3][2] + base - coeff[3][0]) >> 1;
@@ -285,24 +281,22 @@ void __cdecl Image_DecodeWavelet(GfxRawImage *image, t5::GfxImageFileHeader *ima
 	}
 
 	unsigned __int8 mipLevel = decode.mipLevel;
-	for (decode.data = (const char *)data; decode.mipLevel >= 0; --decode.mipLevel)
+	for (decode.data = (const char *)data; decode.mipLevel >= 0; mipLevel = --decode.mipLevel)
 	{
 		int mipWidth = max(decode.width >> mipLevel, 1);
 		int mipHeight = max(decode.height >> mipLevel, 1);
-
+	
 		unsigned int mipSize = bytesPerPixel * mipWidth * mipHeight;
-
+	
 		for (int faceIndex = 0; faceIndex < faceCount; faceIndex++)
 		{
 			from[faceIndex] = to[faceIndex];
 			to[faceIndex] = &pixels[faceIndex][totalSize] - mipSize;
-
+	
 			Wavelet_DecompressLevel((char*)from[faceIndex], (char*)to[faceIndex], &decode);
 			if (faceIndex == 0 && decode.mipLevel == 0)
 				Image_CopyBitmapData(to[faceIndex], image, imageFile);
 		}
-
-		mipLevel = decode.mipLevel - 1;
 	}
 
 	for (int i = faceCount - 1; i >= 0; --i)
