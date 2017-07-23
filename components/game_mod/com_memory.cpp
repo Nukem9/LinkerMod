23771bb69e844b0b22e326d3296f3300bdecec5f
Nukem <Nukem@outlook.com>
2017-07-23 04:41:54 -0400

    game_mod: Z_VirtualAlloc/Z_VirtualFree

--- components/game_mod/com_memory.cpp
+++ components/game_mod/com_memory.cpp
@@ -1,5 +1,11 @@
 #include "stdafx.h"
 
+// /universal/com_memory.cpp:351
+void Z_VirtualFreeInternal(void *ptr)
+{
+	VirtualFree(ptr, 0, MEM_RELEASE);
+}
+
 // /universal/com_memory.cpp:1220
 void *Hunk_Alloc(int size, const char *name, int type)
 {
@@ -17,6 +23,58 @@ void *Hunk_AllocAlign(int size, int alignment, const char *name, int type)
 	ASSERT(alignment <= HUNK_MAX_ALIGNMENT);
 
 	return ((void *(__cdecl *)(int, int))0x0043EEB0)(size, alignment);
+}
+
+// /universal/com_memory.cpp:1737
+void *Z_VirtualAlloc(int size, const char *name, int type)
+{
+	return ((void *(__cdecl *)(int))0x00696F70)(size);
+}
+
+// /universal/com_memory.cpp:1792
+void Z_VirtualFree(void *ptr, int type)
+{
+#if 0
+	if (ptr)
+	{
+		MEMORY_BASIC_INFORMATION info;
+		memset(&info, 0, sizeof(MEMORY_BASIC_INFORMATION));
+
+		int freeSize = 0;
+		int byteCount = VirtualQuery(ptr, &info, sizeof(MEMORY_BASIC_INFORMATION));
+
+		if (byteCount)
+		{
+			char *base = info.AllocationBase;
+
+			ASSERT(base == ptr);
+			ASSERT(info.BaseAddress == ptr);
+
+			for (char *rangePtr = (char *)ptr;; rangePtr += info.RegionSize)
+			{
+				byteCount = VirtualQuery(rangePtr, &info, sizeof(MEMORY_BASIC_INFORMATION));
+
+				if (!byteCount)
+					break;
+
+				ASSERT(info.BaseAddress == rangePtr);
+
+				if (info.AllocationBase != base)
+					break;
+
+				ASSERT(info.RegionSize > 0);
+
+				if (info.State == MEM_COMMIT)
+					freeSize += info.RegionSize;
+			}
+
+			// if (freeSize)
+			//	track_z_decommit(freeSize, type);
+		}
+	}
+#endif
+
+	Z_VirtualFreeInternal(ptr);
 }
 
 // /universal/com_memory.cpp:1894
--- components/game_mod/com_memory.h
+++ components/game_mod/com_memory.h
@@ -2,7 +2,10 @@
 
 #define HUNK_MAX_ALIGNMENT 4096
 
+void Z_VirtualFreeInternal(void *ptr);
 void *Hunk_Alloc(int size, const char *name, int type);
 void *Hunk_AllocAlign(int size, int alignment, const char *name, int type);
+void *Z_VirtualAlloc(int size, const char *name, int type);
+void Z_VirtualFree(void *ptr, int type);
 void Z_Free(void *ptr, int type);
 void *Z_Malloc(int size, const char *name, int type);
