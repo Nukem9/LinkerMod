99f7c8359cb0cf42047acedd116ce83accbe0697
Nukem <Nukem@outlook.com>
2017-08-02 17:21:35 -0400

    game_mod: Rewrite SV_ReceiveStats

--- components/game_mod/msg_mp.h
+++ components/game_mod/msg_mp.h
@@ -1,5 +1,24 @@
 #pragma once
 
+enum netadrtype_t
+{
+	NA_BOT = 0x0,
+	NA_BAD = 0x1,
+	NA_LOOPBACK = 0x2,
+	NA_BROADCAST = 0x3,
+	NA_IP = 0x4,
+};
+
+struct netadr_t
+{
+	netadrtype_t type;
+	char ip[4];
+	unsigned __int16 port;
+	int addrHandleIndex;
+};
+STATIC_ASSERT_OFFSET(netadr_t, ip, 0x4);
+STATIC_ASSERT_SIZE(netadr_t, 0x10);
+
 enum netsrc_t
 {
 	NS_CLIENT1 = 0x0,
@@ -32,6 +51,10 @@ VANILLA_FUNC(MSG_WriteByte, void(__cdecl*)(msg_t *msg, int c), 0x004EFD70);
 VANILLA_FUNC(MSG_WriteShort, void(__cdecl*)(msg_t *msg, int c), 0x005F4710);
 VANILLA_FUNC(MSG_WriteLong, void(__cdecl*)(msg_t *msg, int c), 0x0048E070);
 
+VANILLA_FUNC(MSG_ReadByte, int(__cdecl*)(msg_t *msg), 0x00626990);
+VANILLA_FUNC(MSG_ReadShort, int(__cdecl*)(msg_t *msg), 0x005F46D0);
+VANILLA_FUNC(MSG_ReadData, int(__cdecl*)(msg_t *msg, void *data, int len), 0x00519DD0);
+
 // MSG_WriteString takes up 1 to 32 bytes depending on the length of the string
 VANILLA_FUNC(MSG_WriteString, void(__cdecl*)(msg_t *sb, const char *s), 0x00430420);
 
@@ -40,3 +63,8 @@ VANILLA_FUNC(MSG_WriteBitsCompress, int(__cdecl*)(bool trainHuffman, const char 
 
 VANILLA_FUNC(MSG_ClearLastReferencedEntity, void(__cdecl*)(msg_t *msg), 0x0063A210)
 VANILLA_FUNC(MSG_WriteEntity, int(__cdecl*)(struct SnapshotInfo_s *snapInfo, msg_t *msg, const int time, struct entityState_s *from, struct entityState_s *to, int flags), 0x00531680);
+
+static bool NET_OutOfBandPrint(netsrc_t sock, netadr_t adr, const char *data)
+{
+	return ((bool(__cdecl *)(netsrc_t, netadr_t, const char *))0x00472850)(sock, adr, data);
+}
--- components/game_mod/sv_main_mp.cpp
+++ components/game_mod/sv_main_mp.cpp
@@ -1,5 +1,98 @@
 #include "stdafx.h"
 
+// /server_mp/sv_main_mp.cpp:542
+client_t *SV_FindClientByAddress(netadr_t from, int qport)
+{
+	return ((client_t *(__cdecl *)(netadr_t, int))0x005BD110)(from, qport);
+}
+
+// /server_mp/sv_main_mp.cpp:???
+void LiveStats_DecompressStats(char *statsDestBuffer, int numCompressedPackets)
+{
+	((void(__cdecl *)(char *, int))0x0047F300)(statsDestBuffer, numCompressedPackets);
+}
+
+// /server_mp/sv_main_mp.cpp:???
+bool SV_DoWeHaveAllStatsPackets(client_t *cl, int numCompressedPackets)
+{
+	if (numCompressedPackets <= 0)
+		return true;
+
+	for (int i = 0; *(__int64 *)((int)cl + 0x3EE60) & (1i64 << i);)
+	{
+		if (++i >= numCompressedPackets)
+			return true;
+	}
+
+	return false;
+}
+
+// /server_mp/sv_main_mp.cpp:???
+void SV_ReceiveStats(netadr_t addr, msg_t *msg)
+{
+	int qport = MSG_ReadShort(msg);
+
+	client_t *cl = SV_FindClientByAddress(addr, qport);
+
+	if (!cl)
+	{
+		Com_PrintWarning(
+			15,
+			"Received stats packet from unknown remote client %u.%u.%u.%u\n",
+			addr.ip[0],
+			addr.ip[1],
+			addr.ip[2],
+			addr.ip[3]);
+		return;
+	}
+
+	int packetNum = MSG_ReadByte(msg);
+	int numCompressedPackets = MSG_ReadByte(msg);
+
+	// Don't do anything if we already have the full stats
+	if (SV_DoWeHaveAllStatsPackets(cl, numCompressedPackets))
+		return;
+
+	// Packet index sanity check
+	if (packetNum < 0 || packetNum >= 1)
+	{
+		Com_PrintWarning(15, "Invalid stat packet %i of stats data\n", packetNum);
+		return;
+	}
+
+	Com_Printf(15, "Received packet %i of %i of stats data\n", packetNum, numCompressedPackets);
+
+	int start = 1156 * packetNum;
+	int size = 1024 - start;
+
+	if (size > 1156)
+		size = 1156;
+
+	ASSERT(size > 0);
+	ASSERT_MSG_VA(start + size <= (1 * 1024), "Start is %i, size is %i", start, size);
+
+	MSG_ReadData(msg, (void *)(start + (int)cl + 0x3E9D5), size);
+
+	*(__int64 *)((int)cl + 0x3EE60) |= 1i64 << packetNum;	// statPacketsReceived
+	*(DWORD *)((int)cl + 0x11450) = *(DWORD *)0x0286D014;	// lastPacketTime
+	*(DWORD *)((int)cl + 0x3EE58) = 0;						// statsSentIndex
+	*(DWORD *)((int)cl + 0x3EE5C) = 0;						// statsModified
+
+	memset((void *)((int)cl + 0x3EDD5), 0, 128);			// modifiedStatBytes
+
+	// We updated the flags this time, so check again
+	if (SV_DoWeHaveAllStatsPackets(cl, numCompressedPackets))
+	{
+		LiveStats_DecompressStats((char *)((int)cl + 0x3E9D5), numCompressedPackets);
+
+		// These bitflags indicate we have all packets
+		*(__int64 *)((int)cl + 0x3EE60) |= 1;
+	}
+
+	__int64 neededStatBits = ~*(__int64 *)((int)cl + 0x3EE60) & ((1i64 << numCompressedPackets) - 1);
+	NET_OutOfBandPrint(NS_CLIENT1, addr, va("statresponse %lld", neededStatBits));
+}
+
 // /server_mp/sv_main_mp.cpp:2086
 void SV_CheckTimeouts()
 {
