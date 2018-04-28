a718c2eda1eaa3fa3416819033139ed553f342e1
SE2Dev <brokensilencedev@gmail.com>
2018-04-28 14:12:25 -0400

    Fix Asset_Util Pattern Searcher

--- components/asset_util/common/ff.cpp
+++ components/asset_util/common/ff.cpp
@@ -21,10 +21,21 @@ enum class RAWFILE_TYPE
 
 // Search from _First to _Last for a match against any of the scan patterns
 // returns _Last if no match was found, or an iterator to the match if successful
-// Signature points to the matched signature if there was one
+// Signature points to the matched signature if there was one, or NULL if there was no match
 template<class _FwdIt1>
 inline _FwdIt1 PatternSearch(_FwdIt1 _First, _FwdIt1 _Last, const std::vector<std::vector<BYTE>>& scanList, const std::vector<BYTE>** Signature)
 {
+	// Check to see if the iterator refers to a value that matches the given pattern
+	auto IsMatch = [](_FwdIt1 iter, decltype(scanList[0]) pattern) -> bool
+	{
+		for (size_t i = 0; i < pattern.size(); i++)
+		{
+			if (iter[i] != pattern[i])
+				return false;
+		}
+		return true;
+	};
+
 	// Check each position in the iterable to see if it matches any of the patterns in the scanlist
 	for (_FwdIt1 _Iter = _First; _Iter != _Last; _Iter = std::next(_Iter))
 	{
@@ -36,18 +47,11 @@ inline _FwdIt1 PatternSearch(_FwdIt1 _First, _FwdIt1 _Last, const std::vector<st
 				continue;
 			}
 
-			// Check each value in the pattern
-			for (size_t i = 0; i < pattern.size(); i++)
+			if (IsMatch(_Iter, pattern))
 			{
-				if (_Iter[i] != pattern[i])
-				{
-					continue;
-				}
+				*Signature = &pattern;
+				return _Iter;
 			}
-
-			// Match
-			*Signature = &pattern;
-			return _Iter;
 		}
 	}
 
@@ -357,8 +361,8 @@ void PerformLookup(BYTE *Buffer, std::vector<BYTE>& Data, std::vector<std::vecto
 		// Search for the pattern
 		auto ret = PatternSearch(scanStart, Data.end(), scanList, &Signature);
 
-		// If we didn't find a match, exit
-		if (ret == Data.end())
+		// If we didn't find a match anywhere in the data, exit
+		if (ret == Data.end() || !Signature)
 			break;
 
 		// Convert match index to a real address
