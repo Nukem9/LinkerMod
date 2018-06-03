0e6bb4f06f9f180ceae68dd46ad34f283000cd96
SE2Dev <brokensilencedev@gmail.com>
2018-06-03 13:11:14 -0400

    Tweak VANILLA_FUNC()

--- components/game_mod/vanilla.h
+++ components/game_mod/vanilla.h
@@ -5,4 +5,4 @@
 #define VANILLA_DVAR(NAME, ADDRESS) static dvar_s*& NAME = *(dvar_s **)ADDRESS;
 #define VANILLA_VALUE(NAME, TYPE, ADDRESS) static TYPE& NAME = *(TYPE*)ADDRESS;
 
-#define VANILLA_FUNC(NAME, PTYPE, ADDRESS) static auto NAME = (PTYPE)ADDRESS;
+#define VANILLA_FUNC(NAME, PTYPE, ADDRESS) static auto NAME = (PTYPE)ADDRESS; namespace func_types { using NAME##_t = PTYPE; }
