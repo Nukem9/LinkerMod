611f552dd12448961f897df6871b2170939389c1
SE2Dev <brokensilencedev@gmail.com>
2018-06-03 12:40:18 -0400

    Cleanup Client / Server Mod Verification; Increment GM_NET_VERSION
    GM_NET_VERSION must be incremented because the maximum value length for MatchMakingInfo::m_membermod has been increased by 1 character

--- components/game_mod/live_win.cpp
+++ components/game_mod/live_win.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS 1
 #include "stdafx.h"
 
 const int g_protocolVersion = 3142;
@@ -16,13 +17,72 @@ bool Com_SessionMode_IsZombiesGame()
 	return zombiemode->current.enabled;
 }
 
+//
+// Assigns a given value to a buffer and returns the passed offset incremented by
+// the number of elements that were consumed or returns -1 if the buffer size was exceeded
+//
+template<typename ValueType, typename ElemType, size_t N>
+static size_t __assign_reserved(ElemType(&buf)[N], size_t offset, ValueType val)
+{
+	static_assert(sizeof(ValueType) % sizeof(ElemType) == 0, "Invalid ValueType / ElemType combination!");
+
+	size_t result = offset + sizeof(val);
+
+	// If passed an invalid offset, abort
+	if (offset < 0)
+		return -1;
+
+	// If the new offset would exceed the buffer size, abort
+	if (result > N - 1)
+		return -1;
+
+	*reinterpret_cast<ValueType*>(buf + offset) = val;
+	return result;
+}
+
+static bool Session_ResolveModPattern(char(&dst)[_countof(MatchMakingInfo::m_membermod)], const char* fs_game)
+{
+	//
+	// Assign any reserved values
+	//
+	size_t offset = 0;
+	offset = __assign_reserved(dst, offset, (BYTE)GM_NET_VERSION);
+
+	ASSERT(offset >= 0 && offset < _countof(MatchMakingInfo::m_membermod));
+
+	// NOTE: snprintf() returns the number of characters (excluding the null term) that would have been written
+	//       if buf_size was large enough, a null term is always added either way
+	auto buf_size = _countof(dst) - offset;
+
+	auto n = _snprintf(dst + offset, buf_size, "%s", fs_game);
+
+	if (n + offset > _countof(dst) - 1)
+	{
+#if DEVELOPMENT
+		Com_Error(0,
+			"Mod pattern requires %d bytes, exceeds maximum of %d bytes\n"
+			"  metadata: (%d bytes)\n"
+			"  fs_game:  (%d bytes) '%s'",
+
+			n + offset, _countof(dst) - 1,
+			offset,
+			n, fs_game);
+#else
+		Com_Error(0, "fs_game '%s' requires %d bytes when only %d are available", fs_game, n, _countof(dst) - offset - 1);
+#endif
+		return false;
+	}
+
+	return true;
+};
+
 void __cdecl Session_Modify_Fix(const int localControllerIndex, void *session, const int flags, const int publicSlots, const int privateSlots)
 {
 	MatchMakingInfo* g_matchmakingInfo = *(MatchMakingInfo**)0x03879A24;
-	
-	g_matchmakingInfo->m_membermod[0] = GM_NET_VERSION;
-	sprintf_s(g_matchmakingInfo->m_membermod + 1, 32, "%.*s", 30, Dvar_GetString("fs_game"));
-	//printf("Setting membermod to %s using net version %d\n",Dvar_GetString("fs_game"), LM_NET_VERSION);
+
+	Session_ResolveModPattern(g_matchmakingInfo->m_membermod, Dvar_GetString("fs_game"));
+
+	// printf("Setting membermod to %s using net version %d\n", Dvar_GetString("fs_game"), GM_NET_VERSION);
 
 	return Session_Modify(localControllerIndex, session, flags, publicSlots, privateSlots);
 }
@@ -34,9 +94,8 @@ int SV_UpdateCategory()
 
 int Live_ClientModMatchesServerMod()
 {
-	char c_membermod[32] = "\0";
-	c_membermod[0] = GM_NET_VERSION;
-	sprintf_s(c_membermod + 1, 32, "%.*s", 30, Dvar_GetString("fs_game"));
+	char c_membermod[_countof(MatchMakingInfo::m_membermod)] = "\0";
+	Session_ResolveModPattern(c_membermod, Dvar_GetString("fs_game"));
 
 	if (strcmp(mminfo->m_membermod, c_membermod) != 0)
 	{
