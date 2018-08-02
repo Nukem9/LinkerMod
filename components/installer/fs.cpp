d422d7757d30e7ae2df4248947f220f3c906285a
SE2Dev <brokensilencedev@gmail.com>
2018-08-01 20:43:45 -0400

    Improved Path Normalization

--- components/installer/fs.cpp
+++ components/installer/fs.cpp
@@ -17,9 +17,37 @@ namespace FS
 		return true;
 	}
 
+	std::string NormalizePath(const std::string& path)
+	{
+		std::string out = "";
+		out.reserve(path.size() + 1);
+
+		char prev = '\0';
+		for (char c : path)
+		{
+			if (c == '/' || c == '\\')
+			{
+				c = '\\';
+				if (c == prev)
+				{
+					continue;
+				}
+			}
+
+			out.push_back(prev = c);
+		}
+
+		// Remove trailing delimiters
+		out.erase(std::find_if(out.rbegin(), out.rend(), [](int ch) {
+			return ch != '\\';
+		}).base(), out.end());
+
+		return out;
+	}
+
 	// TODO: Properly check this
 	std::string JoinPath(const std::string& path1, const std::string& path2)
 	{
-		return path1 + "\\" + path2;
+		return NormalizePath(path1 + "\\//\\//\\" + path2);
 	}
 };
