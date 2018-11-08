c65773cc4ef8ebe32245943be929f8a9ad1a606a
SE2Dev <brokensilencedev@gmail.com>
2018-11-08 18:00:11 -0500

    Add Actual Import Patching Code

--- components/bootstrap/pe.cpp
+++ components/bootstrap/pe.cpp
@@ -68,7 +68,7 @@ namespace pe {
 
 		pe_base image(pe_factory::create_pe(pe_file));
 		pe_file.close();
-		
+
 		return handler(image);
 	}
 
@@ -137,13 +137,64 @@ namespace pe {
 	//
 	// Attempt to add an import to the PE at the given filepath
 	//
-	int AddImport(const char* filepath, const char* moduleName, const char* symbol) {
+	int AddImport(const char* filepath, const char* moduleName, const char* symbolName) {
 		return HandleFile<int>(filepath, [=](pe_base& image) {
-			auto booleo = HasImport(image, moduleName, symbol, false);
+			// Don't bother if the import is already present on the binary
+			if(HasImport(image, moduleName, symbolName, false)) {
+				return 0;
+			}
 
-			std::string s = va("%s", booleo ? " ture" : "Faflas");
+			// Get a list of the imported libraries & functions
+			auto libraries = get_imported_functions(image);
 
-			MessageBoxA(0, s.c_str(), s.data(), 0);
+			// Create a new library entry which will import the user specified function
+			import_library userLib;
+			userLib.set_name(moduleName); // Use the user specified module
+
+			// Create an import entry for the user specified function
+			imported_function userFunc;
+			userFunc.set_name(symbolName);	// Use the user defined symbol name
+			userFunc.set_iat_va(0x0);		// Write a non-zero absolute address in the import address table ??????????
+
+			// Add the function to the new library & the new library to the imported library list
+			userLib.add_import(userFunc);	// Actually add the function to the library
+			libraries.push_back(userLib);	// Actually add the library to the image's imports
+
+			// You can edit existing imports
+
+			// But we'll just rebuild the import table
+			// It will be larger than before our editing
+			// so we write it in a new section so that everything fits
+			// (we cannot expand existing sections, unless the section is right at the end of the file)
+			section newImports;
+			newImports.set_name(".hack");	// Assign a name to the given section
+			newImports.readable(true);		// Enable reading
+			newImports.writeable(true);		// Enable writing
+			newImports.get_raw_data().resize(0x1);	// Empty sections are not allowed
+													// so we need to ensure the buffer has a size of at least 1
+
+			// Add the new imports section to the image
+			// A reference to the added section with calculated dimensions is returned
+			section& attachedSection = image.add_section(newImports);
+
+			// Setup the imports rebuilder & rebuild the imports for the image
+			import_rebuilder_settings settings(true, false); // Modify the PE header but do not clear the IMAGE_DIRECTORY_ENTRY_IAT field
+			rebuild_imports(image, libraries, attachedSection, settings);
+
+			// Open the existing binary for writing, and rebuild the PE file
+			const auto mode =	std::ios::out |		// Open with write access
+								std::ios::binary |	// Open in binary mode
+								std::ios::trunc;	// Clear any existing file contents
+			std::ofstream outFile(filepath, std::ios::out | std::ios::binary | std::ios::trunc);
+			if (!outFile) {
+				throw "Could not open file for writing!";
+				return -1;
+			}
+
+			// Rebuild the PE file & write the data to the output file
+			rebuild_pe(image, outFile);
+			outFile.close();
+
 			return 0;
 		});
 	}
